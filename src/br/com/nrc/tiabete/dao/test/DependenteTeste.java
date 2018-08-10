6b640eee4c7eb65d14ee5d219b818947c164f500
Benas <theusbenatti@gmail.com>
2018-08-09 22:29:06 -0300

    REST - Benas - Adicionado todas as classes testes criadas

--- src/br/com/nrc/tiabete/dao/test/DependenteTeste.java
+++ src/br/com/nrc/tiabete/dao/test/DependenteTeste.java
@@ -15,13 +15,16 @@ import br.com.nrc.tiabete.singleton.EntityManagerFactorySingleton;
 
 public class DependenteTeste {
 	public static void main(String[] args) throws CommitException {
-		EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager();
-
+		EntityManager em = EntityManagerFactorySingleton
+				.getInstance().createEntityManager();
+	
+		
 		DependenteDAO dao = new DependenteDAOImpl(em);
 		Dependente dependente = new Dependente();
-
+		
+		
 		TipoDiabete tipoDiabete = new TipoDiabete();
-
+		
 		dependente.setAltura(1.75);
 		dependente.setEmail("matheusbbenatti@gmail.com");
 		dependente.setFoto(null);
@@ -29,12 +32,16 @@ public class DependenteTeste {
 		dependente.setPeso(75);
 		dependente.setSenha("abacaxi123");
 		dependente.setDataCriacao(Calendar.getInstance());
-		dependente.setDataUltimaAlteracao(Calendar.getInstance()); // colocar no DAO
+	    dependente.setDataUltimaAlteracao(Calendar.getInstance()); // colocar no DAO 
 		dependente.setGenero(Genero.MASCULINO);
 		dependente.setMedidor("Accu-Chek");
 		dependente.setModeloMedidor("15198");
-		// dependente.setTipoDiabete(tipoDiabete);
-		dependente.setDataNascimento(new GregorianCalendar(1998, Calendar.APRIL, 27));
-
+		dependente.setTipoDiabete(tipoDiabete);
+		dependente.setDataNascimento(new GregorianCalendar(1998,Calendar.APRIL,27));
+		
+		dao.inserir(dependente);
+		 dao.commit();
+		 em.close();
+		
 	}
 }
