5ed0ebd641ec023611367ffcad0a4bcb92d8f754
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-03 22:04:42 -0300

    REST - Pedrinho - Ajustes nas classes: InsulinaDependente e Item

--- src/br/com/nrc/tiabete/bo/InsulinaDependenteBO.java
+++ src/br/com/nrc/tiabete/bo/InsulinaDependenteBO.java
@@ -4,8 +4,14 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import br.com.nrc.tiabete.dao.DependenteDAO;
+import br.com.nrc.tiabete.dao.InsulinaDAO;
 import br.com.nrc.tiabete.dao.InsulinaDependenteDAO;
+import br.com.nrc.tiabete.dao.impl.DependenteDAOImpl;
+import br.com.nrc.tiabete.dao.impl.InsulinaDAOImpl;
 import br.com.nrc.tiabete.dao.impl.InsulinaDependenteDAOImpl;
+import br.com.nrc.tiabete.entity.Dependente;
+import br.com.nrc.tiabete.entity.Insulina;
 import br.com.nrc.tiabete.entity.InsulinaDependente;
 import br.com.nrc.tiabete.exception.CommitException;
 import br.com.nrc.tiabete.exception.KeyNotFoundException;
@@ -14,17 +20,29 @@ import br.com.nrc.tiabete.singleton.EntityManagerFactorySingleton;
 public class InsulinaDependenteBO {
 	private InsulinaDependenteDAO dao;
 
+	private InsulinaDAO daoInsu;
+
+	private DependenteDAO daoDep;
+
 	public InsulinaDependenteBO() {
 		EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager();
 		dao = new InsulinaDependenteDAOImpl(em);
+		daoInsu = new InsulinaDAOImpl(em);
+		daoDep = new DependenteDAOImpl(em);
 	}
 
 	public List<InsulinaDependente> listar() {
 		return dao.listar();
 	}
 
-	public InsulinaDependente pesquisar(int codigo) {
-		return dao.pesquisar(codigo);
+	public InsulinaDependente pesquisar(int codInsu, int codDep) {
+		Insulina insulina = daoInsu.pesquisar(codInsu);
+		Dependente dependente = daoDep.pesquisar(codDep);
+		InsulinaDependente insuDep = new InsulinaDependente();
+		insuDep.setInsulina(insulina);
+		insuDep.setDependente(dependente);
+
+		return insuDep;
 	}
 
 	public void inserir(InsulinaDependente insuDep) throws CommitException {
@@ -39,8 +57,10 @@ public class InsulinaDependenteBO {
 		dao.commit();
 	}
 
-	public void remover(int codigo) throws CommitException, KeyNotFoundException {
-		dao.remover(codigo);
-		dao.commit();
+	public void remover(int codInsu, int codDep) throws CommitException, KeyNotFoundException {
+		daoInsu.remover(codInsu);
+		daoDep.remover(codDep);
+		daoInsu.commit();
+		daoDep.commit();
 	}
 }
