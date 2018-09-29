adea0e050b50aa5323cb5fcd8800f192c33767ac
Pedrinho <pedrobizzarri@gmail.com>
2018-09-28 21:02:30 -0300

    Implementado pesquisa JPQL para trazer os Dependentes de determinado Responsavel

--- src/br/com/nrc/tiabete/dao/ResponsavelDAO.java
+++ src/br/com/nrc/tiabete/dao/ResponsavelDAO.java
@@ -1,7 +1,11 @@
 package br.com.nrc.tiabete.dao;
 
+import java.util.List;
+
+import br.com.nrc.tiabete.entity.Dependente;
 import br.com.nrc.tiabete.entity.Responsavel;
 
 public interface ResponsavelDAO extends GenericDAO<Responsavel, Integer> {
 
+	List<Dependente> dependentesPorResponsavel(int idResp);
 }
--- src/br/com/nrc/tiabete/dao/impl/ResponsavelDAOImpl.java
+++ src/br/com/nrc/tiabete/dao/impl/ResponsavelDAOImpl.java
@@ -1,13 +1,25 @@
 package br.com.nrc.tiabete.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.nrc.tiabete.dao.ResponsavelDAO;
+import br.com.nrc.tiabete.entity.Dependente;
 import br.com.nrc.tiabete.entity.Responsavel;
 
 public class ResponsavelDAOImpl extends GenericDAOImpl<Responsavel, Integer> implements ResponsavelDAO {
 
 	public ResponsavelDAOImpl(EntityManager em) {
 		super(em);
+	}
+
+	@Override
+	public List<Dependente> dependentesPorResponsavel(int idResp) {
+		return em.createQuery("d.* from ResponsavelDependente rp "
+				+ "join Responsavel r on rp.codigo = r.codigo "
+				+ "join Dependente d on rp.codigo = d.codigo "
+				+ "where r.codigo = :idResp", Dependente.class).setParameter("idResp", idResp)
+				.getResultList();
 	}
 }
