250aeca2757508273c4550da450ab8eb92466010
Pedrinho <pedrobizzarri@gmail.com>
2018-10-15 19:14:14 -0300

    Implementado pesquisa JPQL para media glicemica semanal

--- src/br/com/nrc/tiabete/dao/ValorGlicemicoDAO.java
+++ src/br/com/nrc/tiabete/dao/ValorGlicemicoDAO.java
@@ -1,7 +1,10 @@
 package br.com.nrc.tiabete.dao;
 
+import java.util.Collection;
+
 import br.com.nrc.tiabete.entity.ValorGlicemico;
 
 public interface ValorGlicemicoDAO extends GenericDAO<ValorGlicemico, Integer> {
 
+	Collection<Object[]> mediaGlicemicaSemanal(int codigo);
 }
